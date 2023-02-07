e248683938f9321bc2bc5433f4c08cae55ec964f
Conrad Testagrose <73864022+ctestagrose@users.noreply.github.com>
2023-02-07 15:48:21 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,21 +1,25 @@
-from model_def import ModelDefinition
 import train
 import predict
+import os
 
-cache = 1
-debug = 0
-batch_size = 8
-num_class = 4
-pretrained = True
-dropout_ratio = 0.25
-fc_nodes = 1024
-patch_size = 32
-img_size = 299
-epochs = 50
-model_path = "/content/drive/MyDrive/Breast_Density_Kaggle_Sample_Data/Models/Sample_Model"
-if os.path.isdir(model_path) == False:
-    os.makedirs("/content/drive/MyDrive/Breast_Density_Kaggle_Sample_Data/Models/Sample_Model")
-# model_name = "InceptionV3"
-model_name = 'ViT_Pretrained'
-train(cache, batch_size, img_size, epochs, model_path, model_name, debug)
-predict(model_name, model_path, num_class)
+
+if __name__ == "__main__":
+    cache = 1
+    debug = 0
+    batch_size = 16
+    num_class = 4
+    pretrained = True
+    dropout_ratio = 0.25
+    fc_nodes = 1024
+    patch_size = 32
+    img_size = 299
+    epochs = 100
+    model_path = "./Models/Sample_Model"
+    data_dictionary = "./data_dictionaries/data_dict.json"
+    if os.path.isdir(model_path) == False:
+        os.makedirs("./Models/Sample_Model")
+    # model_name = "InceptionV3"
+    model_name = 'ViT_Pretrained'
+    train.train(num_class, pretrained, cache, data_dictionary, batch_size, img_size, epochs, model_path, model_name, debug)
+    predict.predict(model_name, model_path, num_class, data_dictionary, img_size)
+
